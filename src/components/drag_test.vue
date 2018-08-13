a67ade61719c577b205ec76c4de8c946afd9261d
HomeEmail <591694604@qq.com>
2018-08-13 19:38:17 +0800

    拖

--- src/components/drag_test.vue
+++ src/components/drag_test.vue
@@ -13,12 +13,32 @@
 
   <el-container>
     <el-aside class="left-aside" width="200px">
-      左边
+      <div style="line-height:30px;font-size:14px;color:#666;margin-bottom: 10px;border-bottom: 1px #ddd solid;">画布操作:</div>
+      <el-radio-group v-model="contentBoxIsAllowMove">
+        <el-radio-button :label="true">拖动</el-radio-button>
+        <el-radio-button :label="false">选择</el-radio-button>
+      </el-radio-group>
+      <div style="height:10px;"></div>
+      <el-button @click="resetContentBoxPosition">重置位置</el-button>
+      <div style="height:20px;"></div>
+
+      <div style="line-height:30px;font-size:14px;color:#666;margin-bottom: 10px;border-bottom: 1px #ddd solid;">组件列表:</div>
+      <el-button @click="myElClick" type="text" icon="el-icon-info" :disabled="contentBoxIsAllowMove">文字</el-button>
+      <div style="height:10px;"></div>
+      <el-button type="success" icon="el-icon-success" :disabled="contentBoxIsAllowMove">按钮</el-button>
+      <div style="height:10px;"></div>
+      <el-button type="primary" icon="el-icon-picture" :disabled="contentBoxIsAllowMove">图片</el-button>
+      <div style="height:20px;"></div>
+
     </el-aside>
-    <el-main ref="conterMain" class="center-main">
+    <el-main ref="conterMain" class="">
       <!-- <router-view></router-view> -->
-      <div ref="contentWrapper" :style="contentWrapperCss">
-        <div :style="contentBoxCss">中间</div>
+      <div class="center-main" @mousedown="contentWrapperMouseDown" @mouseup="contentWrapperMouseUp" @mousemove="contentWrapperMouseMove" @mouseout="contentWrapperMouseOut" :style="{cursor: this.contentBoxIsAllowMove?'-webkit-grab':'default',userSelect:this.contentBoxIsAllowMove?'none':'auto'}">
+        <div ref="contentWrapper" :style="contentWrapperCss">
+          <div :style="contentBoxCss">
+            中间<canvas width="200" height="200" ref="canvas1"></canvas>
+          </div>
+        </div>
       </div>
 
     </el-main>
@@ -45,15 +65,69 @@
           padding:'0px',
           width:'100%'
         },
-        contentWrapperCss:{
-          padding: '10px',position: 'relative',width:'1310px'
-        },
-        contentBoxCss:{
-          width:'1280px',height:'720px',boxShadow: '0px 0px 10px #aaa',position:'relative'
-        }
+        dragOrSelect:'选择',
+        contentBoxWidth:1280,
+        contentBoxHeight:720,
+        contentBoxIsAllowMove:true,
+        contentWrapperTop:0,
+        contentWrapperLeft:0,
+        contentWrapperTopNew:0,
+        contentWrapperLeftNew:0,
+        contentWrapperInitTop:0,
+        contentWrapperInitLeft:0,
+        contentBoxIsMoving:false,
+      }
+    },
+    computed:{
+      contentBoxCss:function(){
+        return {width:this.contentBoxWidth+'px',height:this.contentBoxHeight+'px',boxShadow: '0px 0px 10px #aaa',position:'relative', boxSizing: 'border-box'};
+      },
+      contentWrapperCss:function(){
+        return {padding: '10px',position: 'relative',width:(this.contentBoxWidth+30)+'px',position:'absolute',left:this.contentWrapperLeft+'px',top:this.contentWrapperTop+'px', boxSizing: 'border-box',cursor: this.contentBoxIsAllowMove?'-webkit-grab':'default'};
       }
     },
     methods: {
+      myElClick(e){
+        console.log('myElClick:',e);
+      },
+      contentWrapperMouseMove(e){
+        if(this.contentBoxIsAllowMove&&this.contentBoxIsMoving){
+          //console.log('contentWrapperMouseMove:',e);
+          let new_top=e.clientY||e.y;
+          let new_left=e.clientX||e.x;
+          let T=new_top-this.contentWrapperInitTop;
+          let L=new_left-this.contentWrapperInitLeft;
+          if(T==0&&L==0) return ;
+          this.contentWrapperTop=this.contentWrapperTopNew+T;
+          this.contentWrapperLeft=this.contentWrapperLeftNew+L;
+        }
+        return 0;
+
+      },
+      resetContentBoxPosition(e){
+        this.contentWrapperTop=0;
+        this.contentWrapperLeft=0;
+        this.contentWrapperTopNew=0;
+        this.contentWrapperLeftNew=0;
+      },
+      contentWrapperMouseDown(e){
+        if(!!!this.contentBoxIsAllowMove) return 0;
+        this.contentBoxIsMoving=true;
+        this.contentWrapperInitTop=e.clientY||e.y;
+        this.contentWrapperInitLeft=e.clientX||e.x;
+        this.contentWrapperTopNew=this.contentWrapperTop;
+        this.contentWrapperLeftNew=this.contentWrapperLeft;
+        console.log('contentWrapperMouseDown',this.contentBoxIsMoving,e);
+      },
+      contentWrapperMouseUp(e){
+        if(!!!this.contentBoxIsMoving) return 0;
+        this.contentBoxIsMoving=false;
+        console.log('contentWrapperMouseUp',this.contentBoxIsMoving,e);
+      },
+      contentWrapperMouseOut(e){
+        this.contentWrapperMouseUp(e);
+      },
+
       fullScreen() {
         // true全屏，false非全屏 
         if(document.fullscreen||document.mozFullScreen||document.webkitIsFullScreen||document.msFullscreenElement){//退出全屏
@@ -95,7 +169,8 @@
       }
     },
     mounted(){
-
+      //console.log(this.$refs.canvas1);
+      //console.log(this.$refs.canvas1.getContext('2d'));
     },
     created(){
       let that=this;
@@ -159,5 +234,7 @@
 }
 .center-main{
   overflow: auto;
+  position: relative;
+  height: 100%;
 }
 </style>
