96fba690f2a1568edaf3bba39f57b5020456e359
HomeEmail <591694604@qq.com>
2019-03-19 15:52:51 +0800

    添加了数组4级级联测试，和 自定义指令测试

--- src/components/optionMul.vue
+++ src/components/optionMul.vue
@@ -11,12 +11,30 @@
         @change="handleChange">
       </el-cascader>
     </div>
-
+    <hr />
+    <h2>测试数组里的对象属性</h2>
+    <!--v-dom 自定义的全局指令 -->
+    <div class="item" v-dom="item" v-for="(item,index) in options" @click="itemClick(index)" :key="item.value+'-'+1">
+      {{item.label}}
+      <div class="item" v-for="item2 in item.children" :key="item2.value+'-'+2">
+        {{item2.label}}
+        <div class="item" v-for="item3 in item2.children" :key="item3.value+'-'+3">
+          {{item3.label}}
+          <div class="item" v-for="item4 in item3.children" :key="item4.value+'-'+4">
+            {{item4.label}}
+          </div>
+        </div>
+      </div>
+    </div>
+    <el-button type="primary" @click="btClick">改变数组里的第一个对象的label属性</el-button>
+    <el-button type="primary" @click="btClickAdd">向数组里添加一个对象</el-button>
+    <el-button type="primary" @click="btClickChange">对添加的对象改变其label属性</el-button>
 
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
   data () {
@@ -50,7 +68,7 @@ export default {
               children:[
                 {
                   value:'four',
-                  label:'第四级'
+                  label:'第四级侧向导航'
                 }
               ]
             }, {
@@ -70,15 +88,6 @@ export default {
             }, {
               value: 'color',
               label: 'Color 色彩'
-            }, {
-              value: 'typography',
-              label: 'Typography 字体'
-            }, {
-              value: 'icon',
-              label: 'Icon 图标'
-            }, {
-              value: 'button',
-              label: 'Button 按钮'
             }]
           }, {
             value: 'form',
@@ -89,124 +98,6 @@ export default {
             }, {
               value: 'checkbox',
               label: 'Checkbox 多选框'
-            }, {
-              value: 'input',
-              label: 'Input 输入框'
-            }, {
-              value: 'input-number',
-              label: 'InputNumber 计数器'
-            }, {
-              value: 'select',
-              label: 'Select 选择器'
-            }, {
-              value: 'cascader',
-              label: 'Cascader 级联选择器'
-            }, {
-              value: 'switch',
-              label: 'Switch 开关'
-            }, {
-              value: 'slider',
-              label: 'Slider 滑块'
-            }, {
-              value: 'time-picker',
-              label: 'TimePicker 时间选择器'
-            }, {
-              value: 'date-picker',
-              label: 'DatePicker 日期选择器'
-            }, {
-              value: 'datetime-picker',
-              label: 'DateTimePicker 日期时间选择器'
-            }, {
-              value: 'upload',
-              label: 'Upload 上传'
-            }, {
-              value: 'rate',
-              label: 'Rate 评分'
-            }, {
-              value: 'form',
-              label: 'Form 表单'
-            }]
-          }, {
-            value: 'data',
-            label: 'Data',
-            children: [{
-              value: 'table',
-              label: 'Table 表格'
-            }, {
-              value: 'tag',
-              label: 'Tag 标签'
-            }, {
-              value: 'progress',
-              label: 'Progress 进度条'
-            }, {
-              value: 'tree',
-              label: 'Tree 树形控件'
-            }, {
-              value: 'pagination',
-              label: 'Pagination 分页'
-            }, {
-              value: 'badge',
-              label: 'Badge 标记'
-            }]
-          }, {
-            value: 'notice',
-            label: 'Notice',
-            children: [{
-              value: 'alert',
-              label: 'Alert 警告'
-            }, {
-              value: 'loading',
-              label: 'Loading 加载'
-            }, {
-              value: 'message',
-              label: 'Message 消息提示'
-            }, {
-              value: 'message-box',
-              label: 'MessageBox 弹框'
-            }, {
-              value: 'notification',
-              label: 'Notification 通知'
-            }]
-          }, {
-            value: 'navigation',
-            label: 'Navigation',
-            children: [{
-              value: 'menu',
-              label: 'NavMenu 导航菜单'
-            }, {
-              value: 'tabs',
-              label: 'Tabs 标签页'
-            }, {
-              value: 'breadcrumb',
-              label: 'Breadcrumb 面包屑'
-            }, {
-              value: 'dropdown',
-              label: 'Dropdown 下拉菜单'
-            }, {
-              value: 'steps',
-              label: 'Steps 步骤条'
-            }]
-          }, {
-            value: 'others',
-            label: 'Others',
-            children: [{
-              value: 'dialog',
-              label: 'Dialog 对话框'
-            }, {
-              value: 'tooltip',
-              label: 'Tooltip 文字提示'
-            }, {
-              value: 'popover',
-              label: 'Popover 弹出框'
-            }, {
-              value: 'card',
-              label: 'Card 卡片'
-            }, {
-              value: 'carousel',
-              label: 'Carousel 走马灯'
-            }, {
-              value: 'collapse',
-              label: 'Collapse 折叠面板'
             }]
           }]
         }, {
@@ -226,11 +117,32 @@ export default {
         selectedOptions: [],
     }
   },
+  mounted(){
+    console.log('mounted');
+    setTimeout(()=>{
+      this.options[0].children[0].children[0].label='auto二致';
+    },3000);
+  },
   methods: {
     handleChange(value) {
       console.log('handleChange',value);
       console.log('selectedOptions:',this.selectedOptions);
+    },
+    btClick(e){
+      console.log('btClick',e);
+      this.options[0].children[0].children[0].label='二致';
+      console.log('btClick result',this.options);
+    },
+    btClickAdd(e){
+      this.options.push({label:'添加1',value:'tianjia1'+(+new Date()),children:[{label:'添加1-1'+(+new Date()),value:'tianjia1-1'}]});
+    },
+    btClickChange(e){
+      this.options[this.options.length-1].children[0].label='对添加的对象改变其label属性=二致';
+    },
+    itemClick(i){
+      alert(i);
     }
+
   }
 }
 </script>
@@ -250,5 +162,8 @@ li {
 }
 a {
   color: #42b983;
+}
+.item{
+  padding-left: 20px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -11,6 +11,19 @@ Vue.use(Element,{size:'mini'}); //medium small mini
 
 Vue.config.productionTip = false
 
+//指令，将dom绑定到监听的数据对象里,默认是key是el
+Vue.directive('dom', {
+    bind: function (el, binding) { //el监听的dom，binding 当前指令对象
+    	console.log('directive dom binding:',binding);
+        var obj = binding.value;//指令对象里的绑定的数据对象
+        if (obj != null) {
+            var key = Object.keys(binding.modifiers)[0] || "el";
+            Vue.set(obj, key, el);
+        }
+    }
+});
+
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
